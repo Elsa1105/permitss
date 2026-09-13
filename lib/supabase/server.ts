1881776ff2d872cfdc16f26b3a699236595ee4bb
Elsa1105 <elsaameliatampubolon@gmail.com>
2026-09-14 05:06:29 +0800

    revision

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -2,7 +2,7 @@ import { createServerClient, type CookieOptions } from "@supabase/ssr";
 import { cookies } from "next/headers";
 
 export async function createServerSupabase() {
-  const cookieStore = await cookies(); // ✅ WAJIB await
+  const cookieStore = cookies(); // ✅ FIX
 
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
